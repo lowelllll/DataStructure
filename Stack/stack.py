9b693b6715471f224b17b8598b2ea3cfae1b74c0
Lowellll <32219612+lowelllll@users.noreply.github.com>
2018-10-05 23:32:51 +0900

    [Python]Stack 구현

---
+++ Stack/stack.py
@@ -0,0 +1,19 @@
+class ArrayStack:
+
+    def __init__(self):
+        self.data = []
+
+    def size(self):
+        return len(self.data)
+
+    def isEmpty(self):
+        return self.size() == 0
+
+    def push(self, item):
+        self.data.append(item)
+
+    def pop(self):
+        return self.data.pop()
+
+    def peek(self):
+        return self.data[-1]
