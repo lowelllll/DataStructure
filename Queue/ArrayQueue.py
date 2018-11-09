c66f1acbf481550bf44e87bfc934745dddc08bd0
lowelllll <yejin2651@naver.com>
2018-11-09 16:26:31 +0900

    [Python]ArrayQueue

---
+++ Queue/ArrayQueue.py
@@ -0,0 +1,19 @@
+class ArrayQueue:
+
+    def __init__(self):
+        self.data = []
+
+    def size(self):
+        return len(self.data)
+
+    def isEmpty(self):
+        return self.size() == 0
+
+    def enqueue(self, item):
+        self.data.append(item)
+
+    def dequeue(self):
+        return self.data.pop(0)
+
+    def peek(self):
+        return self.data[0]
