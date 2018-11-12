8418a5288227751225753f2cea1744d5ec264b8c
lowelllll <yejin2651@naver.com>
2018-11-12 10:12:57 +0900

    [Python] Heap - 최소 힙, 힙 정렬 오름차순
    최소 힙 구현과 최소 힙을 활용한 힙 정렬

--- Heap/Heap.py
+++ Heap/Heap.py
@@ -52,9 +52,54 @@ class MaxHeap: # 최대 힙
             self.max_heapify(smallest)
 
 
-def heap_sort(unsorted):
+class MinHeap: # 최소 힙
+
+    def __init__(self):
+        self.data = [None]
+
+    def insert(self, item):
+        self.data.append(item)
+        idx = len(self.data)-1
+        parent_idx = idx // 2
+
+        while parent_idx:
+            if self.data[parent_idx] > self.data[idx]:
+                self.data[parent_idx], self.data[idx] = self.data[idx], self.data[parent_idx]
+                idx = parent_idx
+                parent_idx = idx // 2
+            else:
+                break
+
+    def remove(self):
+        if len(self.data) > 1:
+            self.data[1], self.data[-1] = self.data[-1], self.data[1]
+            data = self.data.pop(-1)
+            self.minHeapify(1)
+        else:
+            data = None
+
+        return data
+
+    def minHeapify(self, idx):
+        left = idx * 2
+        right = (idx * 2) + 1
+        biggest = idx
+
+        if len(self.data) > left and self.data[left] < self.data[biggest]:
+            biggest = left
+
+        if len(self.data) > right and self.data[right] < self.data[biggest]:
+            biggest = right
+
+        if biggest != idx:
+            self.data[biggest], self.data[idx] = self.data[idx], self.data[biggest]
+            self.minHeapify(biggest)
+
+
+
+def heap_sort_desc(unsorted):
     """
-    힙을 활용한 정렬 알고리즘
+    MAX 힙을 활용한 정렬 알고리즘
     힙 삽입의 시간 복잡도 O(logn)
     삭제의 시간 복잡도 O(logn)
     => O(nlogn) 복잡도
@@ -74,6 +119,24 @@ def heap_sort(unsorted):
     return sorted
 
 
+def heap_sort_asc(unsorted):
+    """
+    MIN 힙을 활용한 정렬 알고리즘
+    :param unsorted:
+    :return:
+    """
+    h = MinHeap()
+    sorted = []
+
+    for data in unsorted:
+        h.insert(data)
+
+    d = h.remove()
+    while d:
+        sorted.append(d)
+        d = h.remove()
+
+    return sorted
 
 h = MaxHeap()
 
@@ -89,4 +152,22 @@ h.remove()
 print(h.data)
 
 # 힙정렬(내림차순)
-print(heap_sort([4,6,5,7,2,9,1,3,8]))
+print(heap_sort_desc([4,6,5,7,2,9,1,3,8]))
+
+min_heap = MinHeap()
+
+min_heap.insert(80)
+min_heap.insert(11)
+min_heap.insert(2)
+min_heap.insert(1)
+min_heap.insert(13)
+min_heap.insert(90)
+min_heap.insert(76)
+min_heap.insert(44)
+
+min_heap.remove()
+
+print(min_heap.data)
+
+# 힙정렬 (오름차순)
+print(heap_sort_asc([4,6,5,7,2,9,1,3,8]))
